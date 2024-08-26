21fa721de8ee12f9fd9532aa8d7802b32cd26832
Peter Armitage <ficochu84@gmail.com>
2024-08-26 17:34:13 -0300

    fixed card color issue

--- app/components/Infocard.tsx
+++ app/components/Infocard.tsx
@@ -26,9 +26,11 @@ export default function InfoCard({
 	const [imageError, setImageError] = useState(false);
 
 	return (
-		<Card className='overflow-hidden'>
+		<Card className='overflow-hidden bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700'>
 			<CardHeader>
-				<CardTitle>{title}</CardTitle>
+				<CardTitle className='text-gray-900 dark:text-gray-100'>
+					{title}
+				</CardTitle>
 			</CardHeader>
 			<CardContent>
 				{!imageError ? (
@@ -44,14 +46,21 @@ export default function InfoCard({
 						/>
 					</div>
 				) : (
-					<div className='w-full h-48 bg-gray-200 flex items-center justify-center text-gray-500'>
+					<div className='w-full h-48 bg-gray-200 dark:bg-gray-700 flex items-center justify-center text-gray-500 dark:text-gray-400'>
 						Image not available
 					</div>
 				)}
-				<CardDescription className='mt-4'>{description}</CardDescription>
+				<CardDescription className='mt-4 text-gray-600 dark:text-gray-300'>
+					{description}
+				</CardDescription>
 			</CardContent>
 			<CardFooter>
-				<Button onClick={() => window.open(link, '_blank')}>Learn More</Button>
+				<Button
+					onClick={() => window.open(link, '_blank')}
+					className='bg-blue-500 hover:bg-blue-600 text-white dark:bg-blue-600 dark:hover:bg-blue-700'
+				>
+					Learn More
+				</Button>
 			</CardFooter>
 		</Card>
 	);
